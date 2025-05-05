e61d8299ae58c772971853be2ac2ee544de7826d
Mad <marychain121@outlook.com>
2025-05-05 18:42:23 +0900

    add blur effect to the half of AppItemCardFrame

--- app/src/main/java/com/example/composelab/ui/theme/component/practice/FrameElement.kt
+++ app/src/main/java/com/example/composelab/ui/theme/component/practice/FrameElement.kt
@@ -4,7 +4,6 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -13,10 +12,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,20 +27,34 @@ import com.example.composelab.R
 
 @Composable
 fun AppItemCardFrame(modifier: Modifier = Modifier, backgroundImg: Int) {
+    // Load the background once
+    val bitmap = ImageBitmap.imageResource(id = backgroundImg)
+
     Box(
         modifier = modifier
             .border(
                 width = 6.dp,
                 color = Color.Green.copy(green = 0.5f),
                 shape = RoundedCornerShape(8.dp)
-            ),
-        contentAlignment = Alignment.TopStart
+            )
     ) {
         Image(
             painter = painterResource(backgroundImg),
             contentDescription = null,
-            modifier = Modifier.fillMaxWidth(),
-            contentScale = ContentScale.Crop
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxWidth()
+                .fillMaxWidth()
+                .drawWithCache {
+                    val gradient = Brush.verticalGradient(
+                        colors = listOf(Color.Transparent, Color.Green),
+                        startY = size.height / 3f,
+                        endY = size.height
+                    )
+                    onDrawWithContent {
+                        drawContent()
+                        drawRect(gradient)
+                    }
+                }
         )
         Text(
             text = "EDITOR'S CHOICE",
@@ -47,8 +63,8 @@ fun AppItemCardFrame(modifier: Modifier = Modifier, backgroundImg: Int) {
             ),
             color = Color.White,
             modifier = Modifier
-                .clip(RoundedCornerShape(8.dp))// Add padding around the frame
-                .border( // Add a border to create the frame effect
+                .clip(RoundedCornerShape(8.dp))
+                .border(
                     width = 1.dp,
                     color = Color.Green.copy(alpha = 0.5f, green = 0.5f),
                     shape = RoundedCornerShape(8.dp)
@@ -60,19 +76,11 @@ fun AppItemCardFrame(modifier: Modifier = Modifier, backgroundImg: Int) {
                 .padding(8.dp)
         )
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight(0.5f)
-                .align(Alignment.BottomCenter)
-                .blur(15.dp)
-        )
     }
 }
 
-@Preview(showBackground = true, widthDp = 400, heightDp = 250)
+@Preview(widthDp = 400, heightDp = 550)
 @Composable
 fun PreviewFramedCardExample() {
     AppItemCardFrame(backgroundImg = R.drawable.fc2_nature_meditations)
 }
-
